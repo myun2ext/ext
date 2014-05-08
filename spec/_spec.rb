326f23fe212b3c523e1b373cc48c3717e59ee1f0
myun2ext <myun2@mbm.nifty.com>
2014-05-08 13:18:20 +0900

    Update _spec.rb
    Testing successed

--- spec/_spec.rb
+++ spec/_spec.rb
@@ -2,11 +2,12 @@ require './lib/ext/implements/fiddle_by_importer'
 
 describe { it {
 
-class User32
+module User32
   extend Ext::Implements::FiddleByImporter
 
   library "user32"
   declare "MessageBox", ["void*", "const char*", "const char*", "int"], "int"
 end
 
+User32.MessageBox(nil, "aa", "bb", 0)
 }}
